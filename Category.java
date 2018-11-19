eb44d69e4867f170a1cd7804a8f5af9a07563595
robibin <muckenhirn.r@laposte.net>
2018-11-19 13:07:26 +0100

    Create Category.java

---
+++ Category.java
@@ -0,0 +1,35 @@
+package model;
+
+public interface Category {
+
+	
+	/**
+		 * <!-- begin-user-doc -->
+		 * <!--  end-user-doc  -->
+		 * @generated
+		*/
+
+
+		/**
+		 * <!-- begin-user-doc -->
+		 * <!--  end-user-doc  -->
+		 * @generated
+		 * @ordered
+		 */
+		
+		public boolean addcategory(Category category); 
+
+		/**
+		 * <!-- begin-user-doc -->
+		 * <!--  end-user-doc  -->
+		 * @generated
+		 * @ordered
+		 */
+		
+		public boolean removecategory(Category category); 
+		
+
+	}
+
+
+
